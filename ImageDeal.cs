1a8579ee0ab00c7ce7846d6928c921a4d36e84dd
戚晋源 <2166689465@qq.com>
2023-03-28 20:49:35 +0800

    优化了Image的保存和内存释放

--- ImageDeal.cs
+++ ImageDeal.cs
@@ -8,12 +8,13 @@ using System.Runtime.Versioning;
 namespace StandartLibrary
 {
     [SupportedOSPlatform("windows")]
-    public class ImageDeal
+    public class ImageDeal : IDisposable
     {
         private Size _size;
         private float _opacity = 1;
+        private bool disposedValue;
 
-        public Image Image { get; private set; }
+        private Image Image { get; set; }
         public Size Size
         {
             get => _size;
@@ -61,14 +62,52 @@ namespace StandartLibrary
 
         public void SmoothMedian(int k)
         {
-            if(k % 2 == 0) { k -= 1; }
-            if(k < 1) { throw new ArgumentOutOfRangeException("值错误"); }
+            if (k % 2 == 0) { k -= 1; }
+            if (k < 1) { throw new ArgumentOutOfRangeException("值错误"); }
             var t1 = BitmapExtension.ToImage<Bgr, byte>((Bitmap)Image);
             Image.Dispose();
             using var t2 = t1.SmoothMedian(k);
             t1.Dispose();
             Image = t2.ToBitmap();
-            
+
+        }
+
+        public Image Save()
+        {
+            var bim = new Bitmap(Image.Width, Image.Height);
+            using var g = Graphics.FromImage(bim);
+            g.DrawImage(Image, 0, 0, bim.Width, bim.Height);
+            return bim;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: 释放托管状态(托管对象)
+                    Image.Dispose();
+                }
+
+                // TODO: 释放未托管的资源(未托管的对象)并重写终结器
+                // TODO: 将大型字段设置为 null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 仅当“Dispose(bool disposing)”拥有用于释放未托管资源的代码时才替代终结器
+        // ~ImageDeal()
+        // {
+        //     // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
