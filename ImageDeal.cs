edfe7ef356f5874b36ee47902c7b0d08a8b6baad
戚晋源 <2166689465@qq.com>
2023-04-04 16:48:01 +0800

    将imagedeal中的platform修改为使用platform类中的windows

--- ImageDeal.cs
+++ ImageDeal.cs
@@ -7,7 +7,7 @@ using System.Runtime.Versioning;
 
 namespace StandartLibrary
 {
-    [SupportedOSPlatform("windows")]
+    [SupportedOSPlatform(nameof(Platform.windows))]
     public class ImageDeal : IDisposable
     {
         private Size _size;
