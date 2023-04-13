377f7a7cd22bc5b7cd2c25eb7b0f0d4be0156019
戚晋源 <2166689465@qq.com>
2023-04-13 16:27:50 +0800

    .

--- ConvertLibrary.cs
+++ ConvertLibrary.cs
@@ -5,5 +5,7 @@
         public static int Int_Parse(this string str) => Convert.ToInt32(str);
 
         public static double Double_Parse(this string str) => Convert.ToDouble(str);
+
+        public static int Int_Parse(this char c) => Convert.ToInt16(c);
     }
 }
