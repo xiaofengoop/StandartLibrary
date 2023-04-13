c8a8c4b7146d740120476fbaefbe232e0c5e4265
戚晋源 <2166689465@qq.com>
2023-04-13 11:27:19 +0800

    增加了ConvertL方法

--- ConvertLibrary.cs
+++ ConvertLibrary.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StandartLibrary
+﻿namespace StandartLibrary
 {
     public static class ConvertLibrary
     {
         public static int Int_Parse(this string str) => Convert.ToInt32(str);
+
+        public static double Double_Parse(this string str) => Convert.ToDouble(str);
     }
 }
